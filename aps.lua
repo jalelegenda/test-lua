3dc8a69a90455ad20b6beb586fb84fa98faa07f2
Jakov Milić <mydayyourfuneral@yahoo.com>
2018-02-14 00:03:24 +0100

    flow control

--- aps.lua
+++ aps.lua
@@ -1,8 +1,13 @@
 local plane = KEYS[1]
 local planes_spot = redis.call('hget', 'plane:'..plane, 'spot')
 
-if planes_spot ~= nil
+if (tonumber(plane) > 80)
 then
+    return {err="Plane id bigger than 80"}
+elseif (planes_spot)
+then
+    return {err="Plane already has parking spot"}
+else
     math.randomseed(plane)
 
     local spot = math.random(1,99)
